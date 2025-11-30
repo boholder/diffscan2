4476846bfa502a4c0fe06e31a33d4440617e65ce
boholder <bottleholder@anche.no>
2025-11-30 19:02:03 +0800

    feat: -e -w

--- nmap_cmd_gen.py
+++ nmap_cmd_gen.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
 import argparse
+import os
 import random
 import string
+import subprocess
+import sys
 
 
 def random_string(length: int = 8) -> str:
     return ''.join(random.choice(string.ascii_letters) for _ in range(random.randint(length, length + 5)))
 
 
+def execute(cmd: str):
+    # in case forget to add "-w"
+    if os.name == 'nt':
+        cmd = cmd.replace("/tmp", "%localappdata%\\Temp")
+    print(f"Executing: {cmd}")
+    subprocess.run(cmd, shell=True, check=True)
+
+
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(epilog='See: https://github.com/boholder/diffscan2')
     parser.add_argument('--oX-in-tmp', nargs='?', type=str, metavar="PREFIX", const="tmp",
-                        help='add "-oX /tmp/PREFIX<random-string>" option, for hiding output from working directory')
+                        help='add "-oX /tmp/PREFIX<random-string>" option, for hiding output from working directory. Default PREFIX="tmp"')
+    parser.add_argument('-e', action='store_true', help="execute command")
+    parser.add_argument('-w', action='store_true', help="will execute on windows")
     parser.add_argument('nmap_cmd', nargs='+', help='nmap command')
     return parser
 
@@ -23,12 +36,22 @@ def main() -> str:
 
     if args.oX_in_tmp:
         output_path_in_tmp = args.oX_in_tmp + random_string()
-        generated_opts.append(f'-oX /tmp/{output_path_in_tmp}')
+        if args.w:
+            tmp_dir = "%localappdata%\\Temp"
+        else:
+            tmp_dir = "/tmp"
+        generated_opts.append(f'-oX {tmp_dir}/{output_path_in_tmp}')
 
     if "nmap" in args.nmap_cmd:
         args.nmap_cmd.remove("nmap")
-    return "nmap " + " ".join(generated_opts + args.nmap_cmd)
+    _cmd = "nmap " + " ".join(generated_opts + args.nmap_cmd)
+
+    if args.e:
+        execute(_cmd)
+        sys.exit(0)
+
+    return _cmd
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
